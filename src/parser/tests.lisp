a45598d482d15dd35432f5664c3bb0cfa845ad83
Bohdan Khomtchouk <bohdankhomtchouk@UM-31HFG8WM.local>
2017-08-25 19:09:33 -0700

    add parser dir to src

---
+++ src/parser/tests.lisp
@@ -0,0 +1,247 @@
+
+("EGFR expression confers sensitivity to temozolomide in GBM."
+ "
+      {\"cancer\":[\"CANCERTYPE.GBM\"],
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TEMOZOLOMIDE\"]
+      }
+      ")
+
+("In GBM, EGFR expression confers sensitivity to temozolomide."
+ "
+      {\"cancer\":[\"CANCERTYPE.GBM\"],
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TEMOZOLOMIDE\"]
+      }
+      ")
+
+("In a randomized controlled trial involving 100 patients with stage 2 metastatic lung cancer (shrager, 1998), EGFR expression confers sensitivity to dexamethasone."
+ "
+      {\"cancer\":[\"stage\",2,\"metastatic\",\"CANCERTYPE.LUNG\",\"cancer\"],
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":\"shrager, 1998\",
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":\"rct\",
+      \"cases\":100,
+      \"drug(s)\":[\"DEXAMETHASONE\"]
+      }
+      ")
+
+("EGFR expression shows resistance to temozolomide in this elderly man."
+ "
+      {\"cancer\":null,
+      \"relation\":\"resistance_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":\"case_study\",
+      \"cases\":[{\"age\":\"Elderly\",\"gender\":\"MALE\"}],
+      \"drug(s)\":[\"TEMOZOLOMIDE\"]
+      }
+      ")
+
+("NRAS expression conferred resistance to ipilimumab in a 50 year old man with lung cancer."
+ "
+      {\"cancer\":[\"CANCERTYPE.LUNG\",\"cancer\"],
+      \"relation\":\"resistance_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.NRAS\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":null,
+      \"cases\":[{\"age\":50,\"gender\":\"MALE\"}],
+      \"drug(s)\":[\"IPILIMUMAB\"]
+      }
+      ")
+
+("EGFR expression confers sensitivity to paclitaxel in metastatic breast cancer."
+ "
+      {\"cancer\":[\"metastatic\",\"CANCERTYPE.BREAST\",\"cancer\"],
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"PACLITAXEL\"]
+      }
+      ")
+
+("In a randomized controlled trial with 200 patients with stage 2 metastatic melanoma, EGFR expression leads to resistance to cisplatin."
+ "
+      {\"cancer\":[\"stage\",2,\"metastatic\",\"CANCERTYPE.MELANOMA\"],
+      \"relation\":\"resistance_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":\"rct\",
+      \"cases\":200,
+      \"drug(s)\":[\"CISPLATIN\"]
+      }
+      ")
+
+("Keppra induces fatigue and wobbliness"
+ "
+      {\"cancer\":null,
+      \"relation\":[\"INDUCES\"],
+      \"adverse event\":[[\"AE.FATIGUE\",\"and\",\"AE.WOBBLINESS\"]],
+      \"reference\":null,
+      \"molecule(s)\":[],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"KEPPRA\"]
+      }
+      ")
+
+("TMZ lead to toxicity in a clinical trial of 30 patients with GBM."
+ "
+      {\"cancer\":[\"CANCERTYPE.GBM\"],
+      \"relation\":[\"LEAD\"],
+      \"adverse event\":[[\"AE.TOXICITY\"]],
+      \"reference\":null,
+      \"molecule(s)\":[],
+      \"model\":\"trial\",
+      \"cases\":30,
+      \"drug(s)\":[\"TMZ\"]
+      }
+      ")
+
+("Sorafenib significantly accelerated disease progression in a phase II clinical trial of 11 patients with PLGA."
+ "
+      {\"cancer\":[\"CANCERTYPE.PLGA\"],
+      \"relation\":[\"ACCELERATED\"],
+      \"adverse event\":[[\"AE.DISEASE\",\"AE.PROGRESSION\"]],
+      \"reference\":null,
+      \"molecule(s)\":[],
+      \"model\":\"trial\",
+      \"cases\":11,
+      \"drug(s)\":[\"SORAFENIB\"]
+      }
+      ")
+
+("EGFR expression shows resistance to temozolomide in this elderly man."
+ "
+      {\"cancer\":null,
+      \"relation\":\"resistance_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":\"case_study\",
+      \"cases\":[{\"age\":\"elderly\",\"gender\":\"MALE\"}],
+      \"drug(s)\":[\"TEMOZOLOMIDE\"]
+      }
+      ")
+
+("EGFR deletion confers resistance to cisplatin in lung cancer."
+ "
+      {\"cancer\":[\"CANCERTYPE.LUNG\",\"cancer\"],
+      \"relation\":\"resistance_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.DELETION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"CISPLATIN\"]
+      }
+      ")
+
+("Radiotherapy caused major toxicity in a trial of 117 patients with brain cancer."
+ "
+      {\"cancer\":[\"CANCERTYPE.BRAIN\",\"cancer\"],
+      \"relation\":[\"CAUSED\"],
+      \"adverse event\":[[\"AE.MAJOR\",\"AE.TOXICITY\"]],
+      \"reference\":null,
+      \"molecule(s)\":[],
+      \"model\":\"trial\",
+      \"cases\":117,
+      \"drug(s)\":[\"RADIOTHERAPY\"]
+      }
+      ")
+
+("In lung cancer, egfr deletion confers sensitivity tmz and cisplatin."
+ "
+      {\"cancer\":[\"CANCERTYPE.LUNG\",\"cancer\"],
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.DELETION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TMZ\",\"CISPLATIN\"]
+      }
+      ")
+
+("EGFR deletion confers sensitivity tmz and cisplatin in lung cancer."
+ "
+      {\"cancer\":[\"CANCERTYPE.LUNG\",\"cancer\"],
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.DELETION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TMZ\",\"CISPLATIN\"]
+      }
+      ")
+
+("EGFR deletion confers sensitivity tmz and cisplatin."
+ "
+      {\"cancer\":null,
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.DELETION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TMZ\",\"CISPLATIN\"]
+      }
+      ")
+
+("EGFR expression confers sensitivity tmz and cisplatin."
+ "
+      {\"cancer\":null,
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TMZ\",\"CISPLATIN\"]
+      }
+      ")
+
+("BRAF expression confers sensitivity tmz and cisplatin."
+ "
+      {\"cancer\":null,
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.BRAF\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TMZ\",\"CISPLATIN\"]
+      }
+      ")
+
+("EGFR deletion and braf expression confers sensitivity tmz and cisplatin."
+ "
+      {\"cancer\":null,
+      \"relation\":\"sensitivity_to\",
+      \"adverse event\":null,
+      \"reference\":null,
+      \"molecule(s)\":[{\"name\":\"M.EGFR\",\"state\":\"MUTYPEWORD.DELETION\"},\"{\"name\":\"M.BRAF\",\"state\":\"MUTYPEWORD.EXPRESSION\"}],
+      \"model\":null,
+      \"cases\":\"undefined\",
+      \"drug(s)\":[\"TMZ\",\"CISPLATIN\"]
+      }
+      ")
